4cb8faa8748770313bd6ea7a996cd5a2ba94a3bd
stetteh <stettehq@gmail.com>
2016-03-13 11:29:33 -0500

    Add employee to dept

--- EmployeReviews/Department.cs
+++ EmployeReviews/Department.cs
@@ -4,17 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace EmployeReviews
 {
     public class Department
     {
         public string DepartmentName { get; set; }
-        private List<Employee> Employees { get; set; } = new List<Employee>();
+        public List<Employee> Employees = new List<Employee>();
+        
+       
 
         public Department(string departmentname)
         {
             this.DepartmentName = departmentname;
         }
-     
+
+        public void AddEmployee(Employee AnotherEmployee)
+        {
+            Employees.Add(AnotherEmployee);
+        }
+
     }
 }
--- EmployeReviews/UnitTest1.cs
+++ EmployeReviews/UnitTest1.cs
@@ -6,8 +6,10 @@ namespace EmployeReviews
     [TestClass]
     public class UnitTest1
     {
-        //Department dept = new Department();
-        //Employee emp = new Employee();
+        Department dept = new Department("Sales");
+        Department dept2 = new Department("Accounting");
+        Employee emp = new Employee("Seth Quaye", "stettehq@gmail.com", "501-269-4334", 95000m);
+        Employee emp2 = new Employee("John Smith", "jsmith@gmail.com", "501-269-9999", 105000m);
 
         [TestMethod]
         public void CreateNewEmployee()
@@ -24,6 +26,18 @@ namespace EmployeReviews
         {
             var dept = new Department("Sales");
             Assert.AreEqual("Sales", dept.DepartmentName);
+        }
+
+        [TestMethod]
+        public void EmployeeNameTest()
+        {
+            Assert.AreEqual("Seth Quaye", emp.FullName);
+        }
+
+        [TestMethod]
+        public void AddEmployeetoDepartment()
+        {
+            dept2.AddEmployee(emp2);
         }
     }
 }
