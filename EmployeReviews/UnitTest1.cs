827e4f08a92f29cfeee14004184dc94b2ab6af48
stetteh <stettehq@gmail.com>
2016-03-20 11:13:26 -0500

    tests pass

--- EmployeReviews/UnitTest1.cs
+++ EmployeReviews/UnitTest1.cs
@@ -8,9 +8,10 @@ namespace EmployeReviews
     {
         Department dept = new Department("Sales");
         Department dept2 = new Department("Accounting");
-        Employee emp = new Employee("Seth Quaye", "stettehq@gmail.com", "501-269-4334", 95000m);
-        Employee emp2 = new Employee("John Smith", "jsmith@gmail.com", "501-269-9999", 105000m);
-        Employee emp3 = new Employee("Lucky Newton", "lnewton@gmail.com", "501-269-9999", 200000m);
+        Employee emp = new Employee("Seth Quaye", "stettehq@test.com", "501-269-4334", 95000m);
+        Employee emp2 = new Employee("John Smith", "jsmith@test.com", "501-269-9999", 105000m);
+        Employee emp3 = new Employee("Lucky Newton", "lnewton@test.com", "501-269-9999", 200000m);
+        Employee emp4 = new Employee("Lucky Brown", "lbrown@test.com", "901-999-4156", 25000m);
         
 
         [TestMethod]
@@ -64,6 +65,27 @@ namespace EmployeReviews
         public void SatisfactoryPerformance()
         {
             Assert.AreEqual(true, emp.IsSatisfactory());
+        }
+
+        [TestMethod]
+        public void Raise()
+        {
+            emp.Satisfactory = true;
+            emp2.Satisfactory = false;
+            emp3.Satisfactory = true;
+            emp4.Satisfactory = true;
+            dept2.AddEmployee(emp2);
+            dept.AddEmployee(emp4);
+            dept.AddEmployee(emp3);
+            dept2.AddEmployee(emp);
+            dept.GivingRaise(1500m);
+            dept2.GivingRaise(2500m);
+
+            Assert.AreEqual(26500m, emp4.Salary);
+            Assert.AreEqual(105000m, emp2.Salary);
+            Assert.AreEqual(201500m, emp3.Salary);
+            Assert.AreEqual(97500m, emp.Salary);
+
         }
     }
 }
